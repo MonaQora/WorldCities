e4a77ff18e82fc605fd075f134907967e2bfd04f
Mona Qora <mona.abdelrahim@vodafone.com>
2020-01-03 03:31:44 +0200

    initial commit

---
+++ WorldCities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldCities
+//
+//  Created by Mona Qora on 1/3/20.
+//  Copyright © 2020 Mona Qora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
