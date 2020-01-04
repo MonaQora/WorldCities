89dc8cebbd700a71bfc66029f487a07279f1619c
Mona Qora <mona.abdelrahim@vodafone.com>
2020-01-04 04:20:14 +0200

    Add constants

--- WorldCities/Constants/Constants.swift
+++ WorldCities/Constants/Constants.swift
@@ -10,13 +10,14 @@ import Foundation
 
 struct ViewTitles {
     static let cititlsListViewTitle = "Cities"
+    static let cityDetailsViewTitle = "City Details"
 }
 
 struct ErrorDescreption {
     static let defaultErrorMessage = "Somthing went wrong."
-    
     static let wrongJsonPath = "Wrong json path."
     static let failedLoadList = "Failed to load list."
     static let failedParseData = "Failed parse data."
+    static let failedLoadCityCoordinate = "Somthing went wrong, Failed load city location."
 }
 
